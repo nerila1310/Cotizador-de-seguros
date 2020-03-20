15d26a634b7bbff7928efd0e4f758445078d5e0d
Neri-Laredo <nerila1310@gmail.com>
2020-03-20 16:18:15 -0600

    Agregamos animacion

--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { TransitionGroup, CSSTransition } from 'react-transition-group'//para poder utilizarlos instalamos $ npm i react-transition-group
 
 const Mensaje = styled.p`
     background-color: rgb(127, 224, 237);
@@ -32,7 +33,18 @@ const Resultado = ({cotizacion}) => {
         ? <Mensaje>Elige Marca, Año y tipo de Seguro </Mensaje> 
         : (
             <ResultadoCotizacion>
-                <TextoCotizacion>El total es: {cotizacion} </TextoCotizacion>
+                <TransitionGroup
+                    component="p"
+                    className="resultado"
+                >
+                    <CSSTransition
+                        classNames="resultados"
+                        key={cotizacion}
+                        timeout={{enter: 500, exit: 500}}
+                    >
+                        <TextoCotizacion>El total es: {cotizacion} </TextoCotizacion>
+                    </CSSTransition>
+                </TransitionGroup>
             </ResultadoCotizacion>
         ) 
      );
