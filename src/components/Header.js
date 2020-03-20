8be72e904631c39c35ceed1d5af4d90427b75242
Neri-Laredo <nerila1310@gmail.com>
2020-03-20 10:28:35 -0600

    Agregamos eMotion y creamos Styled Components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import styled from '@emotion/styled'; //importamos style de la libreria emotion que descargamos e instalamos con el comandp  $ npm i @emotion/styled @emotion/core
 
+//creamos styled Components 
+const ContenedorHeader = styled.header` 
+    background-color: #26C6DA;
+    padding: 10px;
+    font-weight: bold;
+    color: #FFFFFF;
+`;
+
+const  TextoHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0px;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`;
+//Utilizamos los styled components creados arriba como etiquetas en codigo HTML
 const Header = ({titulo}) => { //recibimos el prop enviado en App para desplegarlo desde aqui
     return ( 
-        <h1> {titulo} </h1> //Etiqueta de Encabezado h1
+        <ContenedorHeader>
+            <TextoHeader> {titulo} </TextoHeader>
+        </ContenedorHeader>
      );
 }
  
